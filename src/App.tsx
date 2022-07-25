f49fd917e03a501f0b527f9e1edeedfda7ba313c
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2022-07-25 09:38:10 +0200

    postavljen projekt, napravljen Navigation i Fotter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DisplayFooter from './components/DisplayFooter/DisplayFooter';
+import MainWrapp from './components/MainWrapp/MainWrapp';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+      <MainWrapp>
+        Main
+        <DisplayFooter/>
+      </MainWrapp>
     </div>
   );
 }
---
+++ src/components/DisplayFooter/DisplayFooter.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import './style.css'
+
+export default function DisplayFooter() {
+  return (
+    <div className="displayFooter" > <h3>Staycation</h3></div>
+  );
+}
